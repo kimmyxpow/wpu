28e5fa22686ccdd36ed155762b054d847c9c7ece
envil a.k.a pow <abinovalfauzi@proton.me>
2024-08-11 03:04:31 +0700

    base

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'raw.githubusercontent.com',
+                port: '',
+                pathname: '/Tarikul-Islam-Anik/Animated-Fluent-Emojis/master/Emojis/**'
+            }
+        ]
+    }
+}
 
-export default nextConfig;
+export default nextConfig
